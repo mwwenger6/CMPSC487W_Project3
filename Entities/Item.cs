98ad1d7467427a03fa6ac8ef7f10192894c7e531
mwwenger6 <109614376+mwwenger6@users.noreply.github.com>
2023-10-09 18:37:23 -0400

    Add files via upload

---
+++ Entities/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMPSC487_Project2.Entities
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("Name")]
+        public string? Name { get; set; }
+        [Column("Description")]
+        public string? Description { get; set; }
+        [Column("FilePath")]
+        public string? FilePath { get; set; }
+    }
+}
